a651a6e5f49005386001a4b203ccaa768fa4bbc8
farodey <farodey@yandex.ru>
2021-11-20 10:47:15 +0300

    основа для графика

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+plt.style.use('seaborn-whitegrid')  # стиль графика
 
+# создаем рисунок fig и 1 график на нём
+fig, ax = plt.subplots()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.show()
